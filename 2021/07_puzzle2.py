79ab8b84b19b5ad99f3c4fa5158788d131d4dd94
SH1RL0CK <sh1rl0ck.h0lm1s@gmail.com>
2021-12-09 21:00:51 +0100

    add solution for 2021/07

---
+++ 2021/07_puzzle2.py
@@ -0,0 +1,13 @@
+content = open("2021/07_puzzle_input.txt", "r").read()
+crabs = list(map(int, content.split(",")))
+
+lowest_fuel = None
+
+for i in range(1, max(crabs)):
+    current_fuel = 0
+    for crab in crabs:
+        current_fuel += sum(range(abs(crab - i) + 1))
+    if lowest_fuel is None or current_fuel < lowest_fuel:
+        lowest_fuel = current_fuel
+
+print(lowest_fuel)
