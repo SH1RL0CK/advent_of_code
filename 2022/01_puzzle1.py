475efb072ba00ad6d5fe6c494814cc772f39e4a1
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2022-12-01 18:49:58 +0100

    add solution for 2022/01

---
+++ 2022/01_puzzle1.py
@@ -0,0 +1,9 @@
+content = open("2022/01_puzzle_input.txt", "r").read().split("\n\n")
+elements = [e.split("\n") for e in content]
+
+cal = []
+
+for e in elements:
+    cal.append(sum([int(c) for c in e]))
+
+print(max(cal))
