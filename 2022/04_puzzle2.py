4b32cbea90e32a53991b7ed80957f7f19c34d816
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2022-12-05 11:23:58 +0100

    add solution for 2022/05

--- 2022/04_puzzle2.py
+++ 2022/04_puzzle2.py
@@ -1,11 +1,30 @@
-content = open("2022/04_puzzle_input.txt", "r").read().splitlines()
+import re
 
-result = 0
+content = open("2022/04_puzzle_input.txt", "r").read().split("\n\n")
 
-for line in content:
-    ranges = line.split(",")
-    range1 = [int(e) for e in ranges[0].split("-")]
-    range2 = [int(e) for e in ranges[1].split("-")]
-    if set(range(range1[0], range1[1] + 1)).intersection(range(range2[0], range2[1] + 1)):
-        result += 1
+stacks_start_state = content[0].splitlines()
+producere = content[1].splitlines()
+
+stacks = []
+
+for crate in stacks_start_state[-1].split("  "):
+    stacks.append([])
+
+
+for line in stacks_start_state[:-1]:
+    for i in range(len(stacks)):
+        crate = line[1 + i * 4]
+        if crate != " ":
+            stacks[i].append(crate)
+
+
+for line in producere:
+    numbers = re.findall("[0-9]+", line)
+    cranes = int(numbers[0])
+    stack1 = int(numbers[1]) - 1
+    stack2 = int(numbers[2]) - 1
+    stacks[stack2] = [*stacks[stack1][:cranes], *stacks[stack2]]
+    stacks[stack1] = stacks[stack1][cranes:]
+
+result = "".join([stack[0] for stack in stacks])
 print(result)
