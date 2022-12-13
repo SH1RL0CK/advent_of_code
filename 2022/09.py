326e7151769ed5d8c00d4eaa6626b3bcd9aa62d3
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2022-12-13 13:44:37 +0100

    add solution for 2022/09 part a

---
+++ 2022/09.py
@@ -0,0 +1,47 @@
+import copy
+
+from aocd.models import Puzzle
+
+import my_grids
+
+puzzle = Puzzle(2022, 9)
+# input_data = open("09_example_input.txt", "r").read().splitlines()
+input_data = puzzle.input_data.splitlines()
+
+grid = my_grids.grid_1
+grid = my_grids.grid_2
+
+
+def part_a():
+    visited_positions = set()
+    head = [0, 0]
+    tail = [0, 0]
+    for line in input_data:
+        motion = line.split(" ")
+        move = [0, 0]
+        match motion[0]:
+            case "R":
+                move = [0, 1]
+            case "L":
+                move = [0, -1]
+            case "U":
+                move = [-1, 0]
+            case "D":
+                move = [1, 0]
+        for _ in range(int(motion[1])):
+            head[0] += move[0]
+            head[1] += move[1]
+            if 2 in [abs(head[0] - tail[0]), abs(head[1] - tail[1])]:
+                tail[0] += move[0]
+                tail[1] += move[1]
+                if head[0] != tail[0] and head[1] != tail[1]:
+                    if move[1] == 0:
+                        tail[1] = head[1]
+                    else:
+                        tail[0] = head[0]
+            visited_positions.add((tail[0], tail[1]))
+    return len(visited_positions)
+
+
+if __name__ == "__main__":
+    puzzle.answer_a = part_a()
